77ff6375b9a780482d4663d22a29b7403f5501cc
Brent Hinks <bhinks@gmail.com>
2021-10-31 08:23:37 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import Response
+
+import pandas as pd
+
+app = Flask(__name__)
+KEY = "e6c5b746bd836be89003f2e23842f473196eddf1"
+
+@app.route("/")
+def title_screen():
+    return render_template("title.html")
+
