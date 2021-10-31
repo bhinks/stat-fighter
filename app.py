2b32d02c172039216e91c2b71cbb185b11fc0393
Brent Hinks <bhinks@gmail.com>
2021-10-31 08:29:11 -0400

    add requirements.txt

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from flask import render_template
 from flask import request
 from flask import Response
 
-import pandas as pd
-
 app = Flask(__name__)
 KEY = "e6c5b746bd836be89003f2e23842f473196eddf1"
 
